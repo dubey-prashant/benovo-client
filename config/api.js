ebe843867fa8805ceff4d171c2c3dbc8350f300e
Prashant Dubey <dubeyprashant583@gmail.com>
2025-04-26 08:02:19 +0530

    fix: Update app.json to correct project name and slug; change API base URL to production

--- config/api.js
+++ config/api.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = 'http://192.168.0.104:8454';
+export const API_BASE_URL = 'https://benovo-server.onrender.com';
+// export const API_BASE_URL = 'http://192.168.0.104:8454'  ;
